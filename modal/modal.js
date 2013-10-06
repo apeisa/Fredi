e0685e20a127fd8ae2f6c52ab0fc67784c52302e
Antti Peisa <antti.peisa@avoine.fi>
2013-10-06 13:50:31 +0300

    Fixed ajax version to behave well in IE and FF
    Also animation after submitting

--- modal/modal.js
+++ modal/modal.js
@@ -4,7 +4,7 @@ var fredi = (function(){
 	
 	iframe=document.createElement('iframe'); 
 	closeIcon=document.createElement('div'); closeIcon.id='frediClose'; 
-	modalDiv=document.createElement('div'); modalDiv.id='frediModal';
+	modalDiv=document.createElement('div'); modalDiv.id='frediModal'; modalDiv.className='loader';
 	outerDiv=document.createElement('div'); outerDiv.id='frediOuter';
 	outerDiv.onclick = function() {fredi.close()};
 	closeIcon.onclick = function() {fredi.close()};
@@ -14,7 +14,6 @@ var fredi = (function(){
 			// Add modal divs to end of the document. These should be styled with css
 			document.body.appendChild(outerDiv);
 			document.body.appendChild(modalDiv);
-			
 			
 			// Load the hidden iframe
 			iframe.src=val;
@@ -27,23 +26,33 @@ var fredi = (function(){
 		},
 		loaded: function () {
 			iframe.style.opacity="1";
+			iframe.style.width="100%";
+			iframe.style.height="100%";
 			modalDiv.className="frediModalOpen";
 			modalDiv.appendChild(closeIcon);
+		},
+		empty: function () {
+			iframe.style.width=0;
+			iframe.style.height=0;
+			modalDiv.removeChild(closeIcon);
+			modalDiv.className='loader';
+
 		},
 		close: function () {
 			modalDiv.removeChild(closeIcon);
 			document.body.removeChild(outerDiv);
 			document.body.removeChild(modalDiv);
-			modalDiv.className="";
+			modalDiv.className="loader";
 		},
 		refresh: function() {
-			 var xhReq = new XMLHttpRequest();
-			 xhReq.open("GET", document.URL, false);
-			 xhReq.send(null);
-			 var serverResponse = xhReq.responseText;
-			 document.body.innerHTML = serverResponse;
+			var xhReq = new XMLHttpRequest();
+			var url = document.URL;
+			xhReq.open("GET", url, false);
+			xhReq.send(null);
+			var serverResponse = xhReq.responseText;
+			var dom = new DOMParser().parseFromString(serverResponse, "text/html");
+			bodyInner = dom.getElementsByTagName( 'body' )[0].innerHTML;
+			document.body.innerHTML = bodyInner;
 		}
 	};
 }());
-
-
